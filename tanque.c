c01858a51cae29f7ec5d379ee89b4a5a1f4cc37a
diegojcmoreira <diego_jesus42@hotmail.com>
2015-06-25 19:32:36 -0300

    Update tanque.c

--- tanque.c
+++ tanque.c
@@ -10,8 +10,8 @@
 
 #include "tanque.h"
 
-#define LARGURA_TANQUE 64
-#define ALTURA_TANQUE  64
+#define LARGURA_TANQUE 140
+#define ALTURA_TANQUE  106
 
 #define DELTA_X (-LARGURA_TANQUE/2)
 #define DELTA_Y (-ALTURA_TANQUE)
@@ -23,9 +23,8 @@ void inicializa_tanque( Tanque* t, int x, int y ) {
   t->max_x = t->posicao_x - DELTA_X;
   t->min_y = t->posicao_x + DELTA_Y;
   t->min_y = t->posicao_y;
-  t->atual = 0;
   
-  t->bitmap[0] = al_load_bitmap("tanque2.png");
+  t->bitmap = al_load_bitmap("tanque2.png");
 
   if( t->bitmap == NULL) {
     puts( "Erro ao carregar o arquivo imagens/tank.png");
@@ -34,21 +33,16 @@ void inicializa_tanque( Tanque* t, int x, int y ) {
 }
 
 void finaliza_tanque( Tanque* t ) {
-  al_destroy_bitmap( t->bitmap[0] );
-  al_destroy_bitmap( t->bitmap[1] );
+  al_destroy_bitmap( t->bitmap );
 }
 
 void desenha_tanque( Tanque* t, DIRECAO direcao ) {
-  int flags = 0;
   
-  if( direcao == DIREITA )
-    flags = ALLEGRO_FLIP_HORIZONTAL;
     
-  al_draw_bitmap( t->bitmap[0], 
+  al_draw_bitmap( t->bitmap, 
       t->posicao_x + DELTA_X, 
       t->posicao_y + DELTA_Y, flags );
   
-  t->atual = (t->atual + 1) % 2;
 }
 
 void move_tanque( Tanque* t, int delta_x, int delta_y ) {
